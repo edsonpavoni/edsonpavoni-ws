6dfbfe3a17b52a9579e5e5c7b35a74a246ee76c3
Edson Pavoni <edsonpavoni@Edsons-MacBook-Pro.local>
2023-05-23 14:39:19 -0300

    new language menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,55 +1,99 @@
 //nav
 const body = document.querySelector('body');
 
-const btnToggle = document.querySelector(".btn-toggle");
-const iconClose = document.querySelector(".icon-close");
-const iconMenu = document.querySelector(".icon-menu");
-const primaryNav = document.querySelector(".primary-navigation");
+const btnMenu = document.querySelector(".primary-nav-btn");
+const btnLang = document.querySelector(".language-nav-btn");
+const btnCloseMenu = document.querySelector(".close-menu");
+const btnCloseLang = document.querySelector(".close-lang");
+
+const primaryNav = document.querySelector(".primary-nav");
+const languageNav = document.querySelector(".language-nav");
+
 const langNavEn = document.querySelector(".lang-navigation-en");
 const langNavBr = document.querySelector(".lang-navigation-br");
+const langNavEs = document.querySelector(".lang-navigation-es");
+const langNavCn = document.querySelector(".lang-navigation-cn");
 
-//hide itens on load
-primaryNav.classList.add('hidden');
-iconClose.classList.add('hidden');
-
-btnToggle.addEventListener('click', () => {
-
-    primaryNav.hasAttribute("data-visible")
-    ? btnToggle.setAttribute("aria-expanded", false)
-    : btnToggle.setAttribute("aria-expanded", true)
-
-    primaryNav.hasAttribute("data-visible")
-    ? primaryNav.classList.add('hidden')
-    : primaryNav.classList.remove('hidden')
-
-    //remove icon X
-    primaryNav.hasAttribute("data-visible")
-    ? iconClose.classList.add('hidden')
-    : iconClose.classList.remove('hidden')
-
-    //remove menu icon
-    primaryNav.hasAttribute("data-visible")
-    ? iconMenu.classList.remove('hidden')
-    : iconMenu.classList.add('hidden')
-
-    //change status
-    primaryNav.toggleAttribute("data-visible");
-
-    //remove scroll
-    body.classList.toggle('menu-open');
-} )
+const menuEn = document.querySelector(".menu-navigation-en");
+const menuBr = document.querySelector(".menu-navigation-br");
+const menuEs = document.querySelector(".menu-navigation-es");
+const menuCn = document.querySelector(".menu-navigation-cn");
 
 
-//brower language detection
+btnMenu.addEventListener('click', () => {
+  primaryNav.setAttribute("data-visible", true);
+})
+
+btnLang.addEventListener('click', () => {
+  languageNav.setAttribute("data-visible", true);
+})
+
+btnCloseMenu.addEventListener('click', () => {
+  primaryNav.setAttribute("data-visible", false);
+})
+
+btnCloseLang.addEventListener('click', () => {
+  languageNav.setAttribute("data-visible", false);
+})
+
+// //brower language detection
+
+//language menu
+langNavEn.classList.add('hidden')
+langNavBr.classList.add('hidden')
+langNavEs.classList.add('hidden')
+langNavCn.classList.add('hidden')
+
+//navigation menu
+menuEn.classList.add('hidden')
+menuBr.classList.add('hidden')
+menuEs.classList.add('hidden')
+menuCn.classList.add('hidden')
 
 //language detection
 if (window.location.toString().includes("/en")) {
     langNavEn.classList.remove('hidden')
     langNavBr.classList.add('hidden')
+    langNavEs.classList.add('hidden')
+    langNavCn.classList.add('hidden')
+
+    menuEn.classList.remove('hidden')
+    menuBr.classList.add('hidden')
+    menuEs.classList.add('hidden')
+    menuCn.classList.add('hidden')
   }
   else if (window.location.toString().includes("/br")) {
     langNavEn.classList.add('hidden')
     langNavBr.classList.remove('hidden')
+    langNavEs.classList.add('hidden')
+    langNavCn.classList.add('hidden')
+
+    menuEn.classList.add('hidden')
+    menuBr.classList.remove('hidden')
+    menuEs.classList.add('hidden')
+    menuCn.classList.add('hidden')
+  }
+  else if (window.location.toString().includes("/es")) {
+    langNavEn.classList.add('hidden')
+    langNavBr.classList.add('hidden')
+    langNavEs.classList.remove('hidden')
+    langNavCn.classList.add('hidden')
+
+    menuEn.classList.add('hidden')
+    menuBr.classList.add('hidden')
+    menuEs.classList.remove('hidden')
+    menuCn.classList.add('hidden')
+  }
+  else if (window.location.toString().includes("/cn")) {
+    langNavEn.classList.add('hidden')
+    langNavBr.classList.add('hidden')
+    langNavEs.classList.add('hidden')
+    langNavCn.classList.remove('hidden')
+
+    menuEn.classList.add('hidden')
+    menuBr.classList.add('hidden')
+    menuEs.classList.add('hidden')
+    menuCn.classList.remove('hidden')
   }
 
 
